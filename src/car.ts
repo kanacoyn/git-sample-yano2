620ca741deb48ce0600ec049b6c58775affeb03e
ryota-09 <n.ryo950911@gmail.com>
2022-02-18 15:03:15 +0900

    フィールド変数speedをnameに変更

--- src/car.ts
+++ src/car.ts
@@ -1,11 +1,12 @@
 export class Car {
-  constructor(private _speed: number) {}
+  //////////中西の変更///////////
+  constructor(private _name: number) {}
 
-  public get speed(): number {
-    return this._speed;
+  public get name(): number {
+    return this._name;
   }
 
-  public set speed(speed: number) {
-    this._speed = speed;
+  public set name(name: number) {
+    this._name = name;
   }
 }
